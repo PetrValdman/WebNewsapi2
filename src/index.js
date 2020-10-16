c97286a26b42549cf26877c7157b6a2bf91c82f8
Peta WHY <petr.valdman@educanet.cz>
2020-10-16 10:39:38 +0100

    m

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+/* window.onload = function() {
+    axios.get('http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-16&sortBy=publishedAt&apiKey=ee0765c8e66c460aa120a123860d453c')
+        .then((res) => {
+            const data = res.data;
+            for(let value of data.articles) {
+                console.log(value.author);
+            }
+        });
+} */
