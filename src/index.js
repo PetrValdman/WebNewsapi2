25ba51cd26a1643ac18a066bcd532d77828e70c8
Peta WHY <petr.valdman@educanet.cz>
2020-11-11 16:01:16 +0000

    m

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
-/* window.onload = function() {
-    axios.get('http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-16&sortBy=publishedAt&apiKey=ee0765c8e66c460aa120a123860d453c')
+ window.onload = function() {
+    axios.get('http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-11&sortBy=publishedAt&apiKey=ee0765c8e66c460aa120a123860d453c')
         .then((res) => {
             const data = res.data;
             for(let value of data.articles) {
-                console.log(value.author);
+                document.write("<div>" + value.title + "</div>");
             }
         });
-} */
+} 
+// http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-16&sortBy=publishedAt&apiKey=ee0765c8e66c460aa120a123860d453c
